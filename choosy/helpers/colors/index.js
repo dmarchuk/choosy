e4bff95ef0b6e9f3be8e62dffc0d54819941ec36
Daniel Marchuk <dmarchuk@me.com>
2017-07-27 00:40:34 +0200

    Added colors - Resolving #5

---
+++ choosy/helpers/colors/index.js
@@ -0,0 +1,12 @@
+'use strict';
+let colors = {
+    highReflectiveWhite: '#f8f9f3',
+    stBarts: '#5c808b',
+    armagnac: '#c48757',
+    tricornBlack: '#2d2d2e',
+    inTheNavy: '#283849',
+    pennywise: '#a35a36',
+    sierraRedwood: '#975e51'
+}
+
+module.exports = colors;
